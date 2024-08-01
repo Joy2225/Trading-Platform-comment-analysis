353e294c70b5d926b99dc8d11a831fe2c45366c9
mehulklh <106767917+mehulklh@users.noreply.github.com>
2024-08-01 20:53:22 +0530

    made basic scraper for reddit using praw

---
+++ scrapers/reddit/scraper.py
@@ -0,0 +1,23 @@
+import praw
+
+import credentials
+
+reddit = praw.Reddit(client_id=credentials.client_id, client_secret=credentials.client_secret,
+    user_agent=credentials.user_agent, username=credentials.username, password=credentials.password)
+
+
+
+def get_comments(url,replace_more_comments=False):
+    submission = reddit.submission(url=url)
+    submission.comments.replace_more(limit=0)
+    return submission.comments.list()
+
+
+
+url = "https://www.reddit.com/r/computerscience/comments/10d016l/looking_for_books_videos_or_other_resources_on/"
+
+submissions = get_comments(url)
+for submission in submissions:
+    print(submission.body)
+
+
